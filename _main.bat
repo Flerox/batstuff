a863ff0538fd374212105ba759b2039cdf0089b4
LIMETTA\felix.bohlin <felix.bohlin@limetta.se>
2017-01-11 16:53:35 +0100

    Timetracker update

--- _main.bat
+++ _main.bat
@@ -98,14 +98,28 @@ if /I "%ttm%" EQU "Y" echo Opening timetracker.bat window. . . && start call _ti
 goto mainmenu
 
 :localTimeTracker
+set date = %date%
+set time = %time%
+set findtext=%date%
+set findfile="_timelog.txt"
+set /a COUNT=0
+for /f %%i in ('findstr %findtext% %findfile%') do (
+  set /a COUNT=COUNT + 1
+)
+if %COUNT% LSS 1 (
+echo %date% >> "_timelog.txt"
+)
 echo.
 echo STARTTIME
 echo %time%
+echo Start: %time% >> "_timelog.txt"
 echo.
 pause > nul | set/p=Press any key to pause session. . . & echo(
 echo.
 echo ENDTIME
 echo %time%
+@(echo End: %time%  && echo.
+) >> "_timelog.txt"
 echo.
 set /P tm=End session [Y/N]?
 if /I "%tm%" EQU "N" echo Fika is over. && goto localTimeTracker
--- _timetracker.bat
+++ _timetracker.bat
@@ -1,16 +1,30 @@
 @echo off
 chcp 1252
-
+color 0b
 :time
+set date = %date%
+set time = %time%
+set findtext=%date%
+set findfile="_timelog.txt"
+set /a COUNT=0
+for /f %%i in ('findstr %findtext% %findfile%') do (
+  set /a COUNT=COUNT + 1
+)
+if %COUNT% LSS 1 (
+echo %date% >> "_timelog.txt"
+)
 echo.
 echo STARTTIME
 echo %time%
+echo Start: %time% >> "_timelog.txt"
 echo.
 pause > nul | set/p=Press any key to pause session. . . & echo(
 echo.
 echo ENDTIME
 echo %time%
+@(echo End: %time%  && echo.
+) >> "_timelog.txt"
 echo.
 set /P tm=End session [Y/N]?
-if /I "%tm%" EQU "N" echo Fika is over. && goto time
-if /I "%tm%" EQU "Y" exit
+if /I "%tm%" EQU "N" echo Fika is over. && goto localTimeTracker
+if /I "%tm%" EQU "Y" goto mainmenu
